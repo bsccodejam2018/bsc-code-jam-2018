6108bb9511ef7a05c142f7e9c04ab9a2e2a77735
Attie Naude <attie.naude@pipware.net>
2018-03-02 11:14:24 +0200

    Implementation of solution to first problem

---
+++ 01-PancakeFlipper/F#/Attie Naude/CodeJam.ProblemOne/PancakeFlipper.fs
@@ -0,0 +1,58 @@
+﻿module PancakeFlipper
+
+type Position = Left|Right
+type Scenario = 
+    {
+        Pancakes: bool[]
+        FlipperWidth: int
+    }
+
+let (|TrimLeadingPositive|_|) (bs: bool[]) =
+    if bs.[0]
+        then Some((Array.splitAt 1 bs) |> snd)
+        else None
+
+let (|TrimTrailingPositive|_|) (bs: bool[]) =
+    if bs.[bs.Length-1]
+        then Some((Array.splitAt (bs.Length-1) bs) |> fst)
+        else None
+
+let rec trimEdges input = 
+    match input with
+    | [||] -> [||]
+    | TrimLeadingPositive bs -> trimEdges bs
+    | TrimTrailingPositive bs -> trimEdges bs
+    | bs -> bs
+
+let getOptimalFlipPosition width pancakes =
+    let left = (Array.splitAt width pancakes) |> fst
+    let right = (Array.splitAt (pancakes.Length - width) pancakes) |> snd
+    let invertedCount = Array.countBy ((=) false)
+
+    if (invertedCount left >= invertedCount right) then
+        Position.Left
+    else
+        Position.Right
+
+let applyFlip width pos arr =
+    let flipValues = Array.map not
+    match pos with
+    | Position.Left ->
+        let sections = arr |> Array.splitAt width
+        Array.concat [fst sections |> flipValues; snd sections]
+    | Position.Right ->
+        let sections = arr |> Array.splitAt (arr.Length-width)
+        Array.concat [fst sections; snd sections |> flipValues]
+
+let calculateFlipCount scenario = 
+    let rec flipPancakesImpl pancakes flipperWidth flipCount =
+        match trimEdges pancakes with
+        | [||] -> Some(flipCount)
+        | ps when ps.Length < flipperWidth -> None
+        | ps -> 
+            let optimalFlipPosition = getOptimalFlipPosition flipperWidth ps
+            let newState = applyFlip flipperWidth optimalFlipPosition ps
+            flipPancakesImpl newState flipperWidth (flipCount+1)
+
+    flipPancakesImpl scenario.Pancakes scenario.FlipperWidth 0
+
