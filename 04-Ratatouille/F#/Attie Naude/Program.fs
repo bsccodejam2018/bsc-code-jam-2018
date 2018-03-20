3d3feced6eaa2745032b8bfcd3047513c5749ff3
Attie Naude <attie.naude@live.com>
2018-03-20 08:15:05 +0200

    Implements solution to Problem #4

---
+++ 04-Ratatouille/F#/Attie Naude/Program.fs
@@ -0,0 +1,53 @@
+﻿open System
+open System.IO
+open Ratatouille
+
+let parseInput lines =
+    let rec processNextScenario (lines: string[]) scenarios =
+        match lines with
+        | [||] -> scenarios
+        | ls -> 
+            let split = ls.[0].Split " "
+            let ingredientCount = split.[0] |> int
+            let packageCount = split.[1] |> int
+
+            let scenario = {
+                RequiredQuantities = ls.[1].Split " " |> Array.map int
+                IngredientPackages = ls.[2..ingredientCount + 1] |> Array.map (fun a -> a.Split " " |> Array.map int)
+            }
+            let remainder = ls.[ingredientCount+2 ..]
+            processNextScenario remainder (scenario :: scenarios)
+
+    processNextScenario lines [] |> List.rev |> List.toArray
+
+let printResult i n =
+    sprintf "Case #%i: %i" (i+1) n
+
+let writeAllLines filePath rows = 
+    File.WriteAllLines(filePath, rows)
+
+[<EntryPoint>]
+let main argv =
+
+    File.ReadAllLines @"..\..\Input\Sample.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.map calculateKitCount
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\Sample.out"
+
+    File.ReadAllLines @"..\..\Input\B-small-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.map calculateKitCount
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\B-small-practice.out"
+
+    File.ReadAllLines @"..\..\Input\B-large-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.map calculateKitCount
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\B-large-practice.out"
+
+    0 // return an integer exit code
---
+++ 04-Ratatouille/F#/Attie Naude/Ratatouille.fs
@@ -0,0 +1,51 @@
+﻿module Ratatouille
+
+open System
+
+type KitInput =
+    {
+        RequiredQuantities : int[]
+        IngredientPackages: int[][]
+    }
+
+let possibleServingCounts requiredQuantity ingredient =
+    let lowerBound = float(ingredient) / float(requiredQuantity) / 1.1 |> ceil |> int
+    let upperBound = float(ingredient) / float(requiredQuantity) / 0.9 |> floor |> int
+    [|lowerBound .. upperBound|]
+
+let calculateKitCount input =
+    let servingCountMatrix = 
+        Array.zip input.RequiredQuantities input.IngredientPackages
+        |> Array.map (fun a -> Array.map (fun b -> possibleServingCounts (fst a) b) (snd a) |> Array.toList)
+        |> Array.map (List.filter (Array.isEmpty >> not))
+        |> Array.toList
+
+    let rec calculateKitCountImpl matrix count =
+        if matrix |> List.exists (List.isEmpty) then
+            count
+        else
+            let sortedRows = 
+                matrix
+                |> List.map (fun a -> a |> List.sortBy (fun b -> (b |> Array.min, b |> Array.length)))
+    
+            let canMakeKit = 
+                sortedRows
+                |> List.map (List.head >> set)
+                |> Set.intersectMany
+                |> Set.isEmpty
+                |> not
+
+            if canMakeKit then
+                let newMatrix =
+                    sortedRows
+                    |> List.map (fun a -> match a with _ :: rem -> rem)
+                calculateKitCountImpl newMatrix count+1
+            else match sortedRows with
+            | [] -> count
+            | h :: rem when h |> List.length = 1 ->
+                count
+            | h :: rem ->
+                let newRow = match h with _ :: r -> r
+                calculateKitCountImpl (newRow::rem) count
+
+    calculateKitCountImpl servingCountMatrix 0
