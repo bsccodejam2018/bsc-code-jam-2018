bb0bcf031ef0086f0a2bad5d1e040d29ee68d637
Attie Naude <attie.naude@live.com>
2018-03-22 10:19:45 +0200

    Normalizes solution directory structure for Problem #2

---
+++ 02-MagicTrick/F#/Attie Naude/MagicTrick.fs
@@ -0,0 +1,12 @@
+﻿module MagicTrick
+
+let getChosenCard (rows: string[]) =
+    let pos1 = rows.[0] |> int
+    let cards1 = rows.[pos1].Split(" ") |> Seq.map int |> Set
+    let pos2 = rows.[5] |> int
+    let cards2 = rows.[pos2+5].Split(" ") |> Seq.map int |> Set
+
+    match Set.intersect cards1 cards2 |> Set.toArray with
+        | [||] -> "Volunteer cheated!"
+        | a when a.Length = 1 -> a.[0] |> string
+        | _ -> "Bad magician!"
---
+++ 02-MagicTrick/F#/Attie Naude/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+open MagicTrick
+
+let processScenarios rows =
+    let rec processNextScenario solutions rows =
+        match rows with
+        | [||] -> solutions
+        | rs ->  getChosenCard rs.[0..9] :: processNextScenario solutions rs.[10..]
+
+    rows |> processNextScenario [] |> List.rev |> List.toArray
+
+let printRow index =
+    sprintf "CASE #%i: %s" (index+1)
+
+let writeAllLines filePath rows = 
+    File.WriteAllLines(filePath, rows)
+
+[<EntryPoint>]
+let main argv =
+    File.ReadAllLines @"..\..\Input\A-small-practice.in"
+    |> Array.skip 1
+    |> processScenarios
+    |> Array.mapi printRow
+    |> writeAllLines @"..\..\Output\A-small-practice.out"
+
+    0 // return an integer exit code
