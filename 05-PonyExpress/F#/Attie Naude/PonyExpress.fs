3c68bf86846eac0b6a16de6aaeae38436e398b86
Attie Naude <attie.naude@live.com>
2018-04-24 10:02:33 +0200

    Implements solution to Problem #5
    This is VERY inefficient though - 6 hour execution time for the large problem set

---
+++ 05-PonyExpress/F#/Attie Naude/PonyExpress.fs
@@ -0,0 +1,84 @@
+﻿module PonyExpress
+
+open System
+
+[<Measure>]
+type km
+[<Measure>]
+type hour
+
+type Horse =
+    {
+        Speed: float<km/hour>
+        Endurance: float<km>
+        VisitedCities: int list
+    }
+
+type CityConnections = Map<int, Map<int, float<km>>>
+
+type ScenarioInput =
+    {
+        CityConnections: CityConnections
+        Horses: Map<int, Horse>
+        SearchPairs: Tuple<int, int>[]
+    }
+
+type State =
+    {
+        CurrentCity: int
+        HorseMap: Map<int, Horse list>
+        TotalTime: float<hour>
+    }
+
+// Small optimization - if 1 horse is both faster and has more endurance then it is the clear choice...
+let availableHorses horses =
+    let fastestHorse = horses |> Seq.sortByDescending (fun a -> a.Speed) |> Seq.head
+    let enduranceChamp = horses |> Seq.sortByDescending (fun a -> a.Endurance) |> Seq.head
+    [fastestHorse; enduranceChamp] |> Set
+
+let citiesReachableByHorse cityConnections horse =
+    cityConnections
+    |> Map.filter (fun cityId distance -> horse.Endurance >= distance && not (List.contains cityId horse.VisitedCities))
+    |> Map.toList
+
+let calculateNewState currentState horse cityId distance =
+    let updatedHorse = { horse with Endurance = horse.Endurance - distance; VisitedCities = cityId :: horse.VisitedCities }
+    let currentCityHorses = currentState.HorseMap.[currentState.CurrentCity] |> List.except [horse]
+    let nextCityHorses = updatedHorse :: currentState.HorseMap.[cityId]
+    let updatedHorseMap = 
+        currentState.HorseMap
+            .Remove(currentState.CurrentCity)
+            .Remove(cityId)
+            .Add(currentState.CurrentCity, currentCityHorses)
+            .Add(cityId, nextCityHorses)
+    { currentState with CurrentCity = cityId; HorseMap = updatedHorseMap; TotalTime = currentState.TotalTime + (distance / horse.Speed) }
+
+
+let yieldBestJourneyTimes (cityConnections: CityConnections) horses startCity endCity =
+    // Mutable values cause me great sadness, but at least it's constrained to 1 function and it's purely for optimization purposes... :|
+    let mutable currentBest = infinity * 1.0<hour>
+
+    let rec yieldBestJourneyTimesImpl (state: State) =
+        seq {
+            if endCity = state.CurrentCity then
+                if state.TotalTime < currentBest then
+                    currentBest <- state.TotalTime
+                    yield state.TotalTime
+            else
+                for horse in availableHorses state.HorseMap.[state.CurrentCity] do
+                    for (cityId, distance) in citiesReachableByHorse cityConnections.[state.CurrentCity] horse do
+                        let newState = calculateNewState state horse cityId distance
+                        if newState.TotalTime < currentBest then
+                            yield! yieldBestJourneyTimesImpl newState
+        }
+
+    yieldBestJourneyTimesImpl { CurrentCity = startCity; HorseMap = horses |> Map.map (fun k v -> [v]); TotalTime = 0.0<hour> }
+
+let calculateMinRideTimes (scenario: ScenarioInput) =
+    let getBestJourneyTime cityPair =
+        yieldBestJourneyTimes scenario.CityConnections scenario.Horses (fst cityPair) (snd cityPair)
+        |> Seq.min
+        |> float
+
+    scenario.SearchPairs
+    |> Array.Parallel.map getBestJourneyTime
---
+++ 05-PonyExpress/F#/Attie Naude/Program.fs
@@ -0,0 +1,72 @@
+﻿open System
+open System.IO
+open PonyExpress
+open System.Globalization
+
+let parseInput lines =
+    let rec processNextScenario (lines: string[]) scenarios =
+        match lines with
+        | [||] -> scenarios
+        | ls -> 
+            let scenarioDimensions = ls.[0].Split " " |> Array.map int
+            let townCount = scenarioDimensions.[0]
+            let caseCount = scenarioDimensions.[1]
+
+            let horses = 
+                ls.[1..townCount]
+                |> Array.mapi (fun townIndex row -> row.Split " " |> Array.map float |> (fun horseValues -> (townIndex + 1, { Endurance = horseValues.[0] * 1.0<km>; Speed = horseValues.[1] * 1.0<km/hour>; VisitedCities = [] })))
+                |> Map.ofArray
+
+            let cityConnections =
+                ls.[1+townCount..townCount*2]
+                |> Array.mapi (fun sourceIndex row -> 
+                    (
+                        sourceIndex+1, 
+                        row.Split " " 
+                        |> Array.mapi (fun destinationIndex value -> ( destinationIndex+1, value |> (fun v -> (float)v * 1.0<km>)) )
+                        |> Array.filter (fun (_, value) -> value > 0.0<km>)
+                        |> Map.ofArray
+                    )
+                )
+                |> Map.ofArray
+
+            let scenarioRows = 
+                ls.[ (1+townCount*2) .. (caseCount+townCount*2) ]
+                |> Array.map (fun row -> row.Split " " |> Array.map int |> (fun vals -> (vals.[0], vals.[1]) ))
+             
+            let currentScenario =
+                    {
+                        CityConnections = cityConnections
+                        Horses = horses
+                        SearchPairs = scenarioRows
+                    }
+            
+            let remainder = ls.[1+caseCount+townCount*2..]
+            processNextScenario remainder (currentScenario :: scenarios)
+
+    processNextScenario lines [] |> List.rev |> List.toArray
+
+let printResult i (r: float[]) =
+    sprintf "Case #%i: %s" (i+1) (String.Join(" ", r |> Seq.map (fun f -> f.ToString(CultureInfo.CreateSpecificCulture("en-GB")))))
+
+let writeAllLines filePath rows = 
+    File.WriteAllLines(filePath, rows)
+
+[<EntryPoint>]
+let main argv =
+
+    File.ReadAllLines @"..\..\Input\C-small-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.Parallel.map calculateMinRideTimes
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\C-small-practice.out"
+
+    File.ReadAllLines @"..\..\Input\C-large-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.Parallel.map calculateMinRideTimes
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\C-large-practice.out"
+
+    0 // return an integer exit code
