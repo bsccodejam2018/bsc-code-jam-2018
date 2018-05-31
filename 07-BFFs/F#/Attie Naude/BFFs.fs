9529a505d1e3224c06fef136865e80fecee5812a
Attie Naude <attie.naude@live.com>
2018-05-31 11:12:10 +0200

    Implements solution to Challenge #7

---
+++ 07-BFFs/F#/Attie Naude/BFFs.fs
@@ -0,0 +1,55 @@
+﻿module BFFs
+
+type Chain = {
+    EndsInMutualFriendship: bool
+    Items: int list
+}
+
+let chainLength chain = chain.Items.Length
+let last2ChildrenInChain chain = chain.Items |> List.rev |> List.take 2 |> List.rev
+let longestChainInGrouping (_, chains) = chains |> Array.sortByDescending chainLength |> Array.head
+
+let getChain (children: Map<int, int>) fromId =
+    let extractChainFromPath path loopStart =
+        let secondLastNode = path |> List.head
+        // Full chain can be used if last 2 children are mutual friends...
+        if children.[loopStart] = secondLastNode then
+            {
+                EndsInMutualFriendship = true
+                Items = path |> List.rev
+            }
+        // Otherwise, only the cycle inside this path is a valid circle...
+        else
+            let loopStartIndex = path |> List.findIndex (fun p -> p = loopStart)
+            {
+                EndsInMutualFriendship = false
+                Items = path.[0..loopStartIndex] |> List.rev
+            }
+
+    let rec getChainImp path nodeId =
+        if path |> List.contains nodeId then
+            extractChainFromPath path nodeId
+        else
+            getChainImp (nodeId :: path) (children.[nodeId])
+
+    getChainImp [] fromId
+
+let getLargestPossibleCircleSize children =
+    let longestIndividualChains =
+        children
+        |> Map.toArray
+        |> Array.map fst
+        |> Array.Parallel.map (getChain children)
+        |> Array.groupBy last2ChildrenInChain
+        |> Array.map longestChainInGrouping
+
+    let combinedChain =
+        longestIndividualChains
+        |> Array.filter (fun c -> c.EndsInMutualFriendship)
+        |> Array.map (fun c -> c.Items)
+        |> List.concat
+        |> List.distinct
+
+    max
+        (longestIndividualChains |> Array.map chainLength |> Array.max)
+        (combinedChain |> List.length)
---
+++ 07-BFFs/F#/Attie Naude/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+open System.IO
+open BFFs
+
+let parseInput lines =
+    let rec processNextScenario (lines: string[]) scenarios =
+        match lines with
+        | [||] -> scenarios
+        | ls -> 
+            let bffMap = ls.[1].Split(" ") |> Array.mapi (fun i bI -> (i+1, bI |> int)) |> Map
+            let remainder = ls.[2..]
+
+            processNextScenario remainder (bffMap :: scenarios)
+
+    processNextScenario lines [] |> List.rev |> List.toArray
+
+let printResult i r =
+    sprintf "Case #%i: %i" (i+1) r
+
+let writeAllLines filePath rows = 
+    File.WriteAllLines(filePath, rows)
+
+[<EntryPoint>]
+let main argv =
+    File.ReadAllLines @"..\..\Input\C-small-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.map getLargestPossibleCircleSize
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\C-small-practice.out"
+
+    File.ReadAllLines @"..\..\Input\C-large-practice.in"
+    |> Array.skip 1
+    |> parseInput
+    |> Array.map getLargestPossibleCircleSize
+    |> Array.mapi printResult
+    |> writeAllLines @"..\..\Output\C-large-practice.out"
+
+    0 // return an integer exit code
